030c5ca287a70c87f709a633030bb6e4287b4b30
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-13 04:27:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -82,7 +82,7 @@ class Owner
   
   # Can list off its pets 
   def list_pets
-    puts "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+    puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
 
